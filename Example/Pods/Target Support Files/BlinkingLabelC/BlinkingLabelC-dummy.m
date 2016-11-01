4e70ddc6d544c9cddc808a225b040595e4be6d0c
Yasodha Rangasamy <yasodha.r@inxsasia.com>
2016-11-01 13:20:57 +0530

    initial commit

---
+++ Example/Pods/Target Support Files/BlinkingLabelC/BlinkingLabelC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BlinkingLabelC : NSObject
+@end
+@implementation PodsDummy_BlinkingLabelC
+@end
---
+++ Example/Pods/Target Support Files/Pods-BlinkingLabelC_Tests/Pods-BlinkingLabelC_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BlinkingLabelC_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BlinkingLabelC_TestsVersionString[];
+
